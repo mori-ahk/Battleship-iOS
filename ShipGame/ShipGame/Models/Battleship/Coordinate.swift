bd0e23ed9561a24784f9a1756b3ef4ce394ff187
Mori Ahmadi <ahmadik.morteza95@gmail.com>
2024-06-03 11:50:48 -0400

    Add new folders to models

---
+++ ShipGame/ShipGame/Models/Battleship/Coordinate.swift
@@ -0,0 +1,25 @@
+//
+//  Coordinate.swift
+//  ShipGame
+//
+//  Created by Mori Ahmadi on 2024-06-03.
+//
+
+import Foundation
+
+struct Coordinate: Equatable {
+    let x: Int
+    let y: Int
+    var state: State = .empty
+    
+    enum State: Int {
+        case empty = 0
+        case occupied
+        case hit
+        case miss
+    }
+    
+    mutating func reset() {
+        state = .empty
+    }
+}
--- ShipGame/ShipGame/Models/Battleship/GameGrid.swift
+++ ShipGame/ShipGame/Models/Battleship/GameGrid.swift
@@ -96,22 +96,6 @@ struct GameGrid: Equatable {
     }
 }
 
-struct Coordinate: Equatable {
-    let x: Int
-    let y: Int
-    var state: State = .empty
-    enum State: Int {
-        case empty = 0
-        case occupied
-        case hit
-        case miss
-    }
-    
-    mutating func reset() {
-        state = .empty
-    }
-}
-
 struct Ship: Equatable {
     var coordinates: [Coordinate]
     
