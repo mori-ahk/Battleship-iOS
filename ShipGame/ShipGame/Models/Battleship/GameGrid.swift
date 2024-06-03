156602b34a3d10c2e8b6e7dcf5ec118133a7550d
Mori Ahmadi <ahmadik.morteza95@gmail.com>
2024-06-03 11:52:14 -0400

    add Ship to its own file

--- ShipGame/ShipGame/Models/Battleship/GameGrid.swift
+++ ShipGame/ShipGame/Models/Battleship/GameGrid.swift
@@ -96,52 +96,6 @@ struct GameGrid: Equatable {
     }
 }
 
-struct Ship: Equatable {
-    var coordinates: [Coordinate]
-    
-    init(coordinates: [Coordinate]) {
-        self.coordinates = coordinates
-        for (index, _) in self.coordinates.enumerated() {
-            self.coordinates[index].state = .occupied
-        }
-    }
-    
-    var isSunk: Bool {
-        coordinates.allSatisfy { $0.state == .hit }
-    }
-    
-    var kind: Kind? {
-        switch coordinates.count {
-        case 4: return .battleShip
-        case 3: return .cruiser
-        case 2: return .destroyer
-        default: return nil
-        }
-    }
-    
-    enum Kind: String, CaseIterable, Identifiable {
-        var id: String { name }
-        case battleShip
-        case cruiser
-        case destroyer
-        
-        var size: Int {
-            switch self {
-            case .battleShip:
-                return 4
-            case .cruiser:
-                return 3
-            case .destroyer:
-                return 2
-            }
-        }
-        
-        var name: String {
-            "\(rawValue.capitalized) (\(size.description))"
-        }
-    }
-}
-
 enum Direction: String, CaseIterable {
     case left
     case up
